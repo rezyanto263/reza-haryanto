c466a2c4618fb0a396e98279efaa75f54a271ac0
Reza Haryanto <rezyanto263@gmail.com>
2025-07-30 22:04:26 +0800

    fix description

--- src/data/profile.js
+++ src/data/profile.js
@@ -15,7 +15,7 @@ export const description = {
     "I'm a Software Engineering student focused on web development, experienced in building functional applications, APIs, and cloud-based systems through both individual and team projects.",
   long: [
     "I'm Muhammad Reza Haryanto, a Software Engineering student at Politeknik Negeri Bali with a growing interest in full-stack development. I’ve been exploring various technologies, mainly Laravel for backend, React.js for frontend, and Express.js for API development. These tools have helped me build several academic and personal projects, such as e-commerce sites, digital queue systems, and booking platforms with integrated QR codes and digital payments.",
-    "Through team-based coursework and projects, I’ve gained experience in working collaboratively, managing tasks, and learning new tools as needed. I’m also part of the Bangkit 2025 program under the Cloud Computing path, which has introduced me to scalable and cloud-based development practices.",
+    "Through team-based coursework and projects, I’ve gained experience in working collaboratively, managing tasks, and learning new tools as needed. I’m also part of the Bangkit 2024 program under the Cloud Computing path, which has introduced me to scalable and cloud-based development practices.",
     "I’m always open to new challenges and opportunities that allow me to learn, contribute, and improve alongside others in a real-world development environment.",
   ],
 };
